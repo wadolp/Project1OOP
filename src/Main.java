3c4bb52d925299226fcc5200cca6be932ab303b4
wadolp <williamisadolph@gmail.com>
2024-09-01 14:33:18 -0500

    main changes

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        denomination[] denoms = {
+            new denomination(100.0, "One Hundred", "One Hundred Pesos", "100.jpg"),
+            new denomination(50.0, "Fifty", "Fifty Pesos", "50.jpg"),
+            new denomination(20.0, "Twenty", "Twenty Pesos", "20.jpg"),
+            new denomination(10.0, "Ten", "Ten Pesos", "10.jpg"),
+            new denomination(5.0, "Five", "Five Pesos", "5.jpg"),
+            new denomination(1.0, "One", "One Peso", "1.jpg"),
+            new denomination(0.25, "Twenty-Five", "Twenty-Five Cents", "25.jpg"),
+            new denomination(0.10, "Ten", "Ten Cents", "10.jpg"),
+            new denomination(0.05, "Five", "Five Cents", "5.jpg"),
+            new denomination(0.01, "One", "One Cent", "1.jpg")
+        };
+        for (denomination d : denoms) {
+            System.out.println(d);
         }
+
     }
 }
